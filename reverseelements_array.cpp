3ed7fe502e62923bb65ecc57803e6fd6dbafcbeb
geetanjali-codes <geetanjalidawra04@gmail.com>
2026-08-17 22:30:39 +0530

    reverseelements_array.cpp

--- reverseelements_array.cpp
+++ reverseelements_array.cpp
@@ -1,24 +1,26 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
-    int i,n;
-    int arr[10];
-    cout<<"Enter number of elements: ";
-    cin>>n;
-    cout<<"Enter elements: ";
-    for(i=0;i<n;i++){
-        cin>>arr[i];
+
+int main() {
+    int arr[] = {10, 20, 30, 40, 50};
+    int n = 5;
+
+    int start = 0;
+    int end = n - 1;
+
+    while (start < end) {
+        // swap arr[start] and arr[end]
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+
+        start++;
+        end--;
     }
 
-    for(i=0; i<n/2; i++){
-        int temp = arr[i];
-        arr[i] = arr[n-1-i];
-        arr[n-1-i] = temp;
+    cout << "Reversed array: ";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
     }
-    cout<<"Reversed Array: ";
-    for(i=0;i<n;i++){
-        cout<<arr[i] <<" ";
-    }
- 
     return 0;
 }
